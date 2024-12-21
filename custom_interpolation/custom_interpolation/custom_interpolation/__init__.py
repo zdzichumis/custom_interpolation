938a55a9d4ed55334b80b9e502075e4bee14a858
zdzichumis <134324988+zdzichumis@users.noreply.github.com>
2024-12-21 13:00:59 +0100

    Delete custom_interpolation directory

--- custom_interpolation/custom_interpolation/custom_interpolation/__init__.py
+++
@@ -1,4 +0,0 @@
-from .polynomial import Polynomial
-from .splains import Splains
-from .lagrange_interpolation import lagrange_interpolation
-from .splains_interpolation import splains_interpolation
